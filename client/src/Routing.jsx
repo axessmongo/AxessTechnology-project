1f7a6180f97e33089ba5220d5f18040a041fe3d7
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2024-01-12 18:38:14 +0530

    Added About Page

--- client/src/Routing.jsx
+++ client/src/Routing.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import About from './About';
 
 function Routing() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Navbar />}>
+                    <Route path="/" element={<About />}>
                         <Route index element = {Home}/>
                     </Route>
                 </Routes>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,17 +5,17 @@ export default function Home() {
     return (
         <div>
             <div id="carouselExampleFade" className="carousel slide carousel-fade">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src={maincarimg1} className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src= "https://img.freepik.com/free-photo/it-specialist-checking-code-computer-dark-office-night_1098-18699.jpg?w=1380&t=st=1704976715~exp=1704977315~hmac=2f8f5383ff88556023a65bad253d1523b19efbd3c0c4b8ebad4e1f76333d93dc" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src= "https://img.freepik.com/free-photo/3d-rendering-biorobots-concept_23-2149524400.jpg?w=1380&t=st=1704976785~exp=1704977385~hmac=e0e6703b6e1d0fec30d919f507bd56d199f05b8de13ca6b8f814d0c0b87d4b6c" className="d-block w-100" alt="..." />
-                    </div>
-                </div>
+                    {/* <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={maincarimg1} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src= "https://img.freepik.com/free-photo/it-specialist-checking-code-computer-dark-office-night_1098-18699.jpg?w=1380&t=st=1704976715~exp=1704977315~hmac=2f8f5383ff88556023a65bad253d1523b19efbd3c0c4b8ebad4e1f76333d93dc" className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src= "https://img.freepik.com/free-photo/3d-rendering-biorobots-concept_23-2149524400.jpg?w=1380&t=st=1704976785~exp=1704977385~hmac=e0e6703b6e1d0fec30d919f507bd56d199f05b8de13ca6b8f814d0c0b87d4b6c" className="d-block w-100" alt="..." />
+                        </div>
+                    </div> */}
                 <button
                     className="carousel-control-prev"
                     type="button"
@@ -35,13 +35,6 @@ export default function Home() {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
-
-            <ul className='myul'>
-                <li>
-                    <a href="">xsaxs</a>
-                </li>
-            </ul>
-
         </div>
     )
 }
