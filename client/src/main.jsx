6e880532341df47ef93bb2402397ed0a41009ef7
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-01-17 18:28:35 +0530

    changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/css/index.css'
+import './assets/css/About.css'
 import './assets/css/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
