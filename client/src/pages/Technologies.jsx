8f4a0206229734e1860ac26e37fb1740478a74d9
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2024-01-23 17:29:59 +0530

    tech-page

--- client/src/pages/Technologies.jsx
+++ client/src/pages/Technologies.jsx
@@ -61,10 +61,10 @@ const Technologies = () => {
 
     return (
         <div className='technology-page'>
-            <div className='tech-tools-section bg-green-1 '>
+            <div className='tech-tools-section bg-green-1 py-5 '>
                 <div className='container'>
                     <div className="row">
-                        <div className="col-md-3 col-lg-5">
+                        <div className="d-none d-lg-block col-lg-5">
                             <div className="row align-items-center justify-content-center">
                                 <div>
                                     <img src={techtools1} style={{ "width": "100%", "height": "100%" }}></img>
@@ -80,10 +80,10 @@ const Technologies = () => {
                                 <h2>Huge Spectrum of Next-Gen Technologies Under One Roof</h2>
                                 <p>Leverage our in-depth technical expertise to bring your stunning ideas to Life.</p>
                             </div>
-                            <div className='row text-white'>
-                                <div className='col-6 col-md-4 col-lg-3'>
-                                    <h4>Frontend</h4>
-                                    <ul>
+                            <div className='row text-white tech-tools-container'>
+                                <div className='col-6 col-md-4 col-lg-3 tech-tools'>
+                                    <h4 className='text-center py-3'>Frontend</h4>
+                                    <ul className="">
                                         <li>HTML/CSS</li>
                                         <li>HTML/CSS</li>
                                         <li>HTML/CSS</li>
@@ -91,9 +91,36 @@ const Technologies = () => {
                                         <li>HTML/CSS</li>
                                     </ul>
                                 </div>
-                                <div className='col-6 col-md-4 col-lg-3'></div>
-                                <div className='col-6 col-md-4 col-lg-3'></div>
-                                <div className='col-6 col-md-4 col-lg-3'></div>
+                                <div className='col-6 col-md-4 col-lg-3 tech-tools'>
+                                    <h4 className='text-center py-3'>Backend</h4>
+                                    <ul className="">
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                    </ul>
+                                </div>
+                                <div className='col-6 col-md-4 col-lg-3 tech-tools'>
+                                    <h4 className='text-center py-3'>Cloud</h4>
+                                    <ul className="">
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                    </ul>
+                                </div>
+                                <div className='col-6 col-md-4 col-lg-3 tech-tools'>
+                                    <h4 className='text-center py-3'>Frontend</h4>
+                                    <ul className="">
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                        <li>HTML/CSS</li>
+                                    </ul>
+                                </div>
 
                             </div>
                            </div>
@@ -101,9 +128,9 @@ const Technologies = () => {
                     </div>
                 </div>
             </div>
-            <div className='technology-section'>
+            <div className='technology-section py-5'>
                 <div className='container technology' >
-                    <div className='primary-header text-center tech-title'>
+                    <div className='primary-header text-center tech-title py-4'>
                         Tech Tools
                     </div>
                     <div className='tech-container'>
