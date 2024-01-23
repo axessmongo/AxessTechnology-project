8590a6ccf24b07daab56cc9069b95ce59d4c2d9b
devbharath21k <devbharath21k@gmail.com>
2024-01-23 15:00:43 +0530

    'new'

--- client/src/Routing.jsx
+++ client/src/Routing.jsx
@@ -5,18 +5,29 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Services from './pages/Services';
 import Technologies from './pages/Technologies';
+import Login from './pages/Login';
+import EmailVerify from './pages/EmailVerify';
+import {  Navigate } from "react-router-dom";
+import Footer from './components/Footer';
 
 function Routing() {
+    const user = localStorage.getItem("token");
     return (
+        
         <div>
+
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Navbar />}>
                         <Route index element={Home} />
                     </Route>
                     <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<Navigate replace to="/login" />} />
+                    <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+                    <Route path="/register" element={<Register />} />
                     <Route path='/services' element={<Services />} />
-                    <Route path="/technologies" element={<Technologies/>}/>
+                    <Route path="/technologies" element={<Technologies />} />
+                    <Route path='/footer' element={<Footer />} />
                 </Routes>
             </BrowserRouter>
         </div>
