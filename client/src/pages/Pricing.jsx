e44aabea6e14de8b63b4a7a19cad625bd36b69a1
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2024-01-24 12:34:16 +0530

    Added new designs

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -8,13 +8,13 @@ function Pricing() {
 	return (
 		<>
 			<Navbar />
-			<Banner h1={'Pricing Plans'} h2={'Get started with 900+ free, customizable templates, a smart drag and drop editor, tools to boost your online presence and more.'} h3={'Grab Our New Offers'}/>
+			<Banner h1={'Pricing Plans'} h2={'Get started with 900+ free, customizable templates, a smart drag and drop editor, tools to boost your online presence and more.'} h3={'Grab Our New Offers'} />
 			<section className='pricing mt-5'>
 				<h1 className='primary-header text-center'>Unlock Our New Features</h1>
 				<div className="container pricing-table">
 					<div className="pricing-card ">
 						<h3 className="pricing-card-header">Basic</h3>
-						<div className="price"><sup>$</sup>15<span>/MO</span></div>
+						<div className="price text-center"><sup>$</sup>15<span>/MO</span></div>
 						<ul>
 							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
 							<li><strong>Hosting - 1</strong> Year FREE</li>
@@ -31,7 +31,7 @@ function Pricing() {
 
 					<div className="pricing-card">
 						<h3 className="pricing-card-header">Pro</h3>
-						<div className="price"><sup>$</sup>30<span>/MO</span></div>
+						<div className="price text-center"><sup>$</sup>30<span>/MO</span></div>
 						<ul>
 							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
 							<li><strong>Hosting - 1</strong> Year FREE</li>
@@ -48,7 +48,7 @@ function Pricing() {
 
 					<div className="pricing-card">
 						<h3 className="pricing-card-header">Premium</h3>
-						<div className="price"><sup>$</sup>50<span>/MO</span></div>
+						<div className="price text-center"><sup>$</sup>50<span>/MO</span></div>
 						<ul>
 							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
 							<li><strong>Hosting - 1</strong> Year FREE</li>
@@ -65,7 +65,7 @@ function Pricing() {
 
 					<div className="pricing-card">
 						<h3 className="pricing-card-header">Elite</h3>
-						<div className="price"><sup>$</sup>80<span>/MO</span></div>
+						<div className="price text-center"><sup>$</sup>80<span>/MO</span></div>
 						<ul>
 							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
 							<li><strong>Hosting - 1</strong> Year FREE</li>
@@ -79,9 +79,13 @@ function Pricing() {
 						</ul>
 						<a href="#" className="order-btn">Order Now</a>
 					</div>
+					{/* <div className='Pay'>
+						<h3>ACCEPTED PAYMENT METHODS</h3>
+
+					</div> */}
 				</div>
 			</section >
-			<Footer/>	
+			<Footer />
 		</>
 
 	)
