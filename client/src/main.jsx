ed2a08fccb2e622bdcea0d7b7ab74cf8c22901d9
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-01-25 16:19:33 +0530

    added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './assets/css/index.css'
 import './assets/css/About.css'
 import './assets/css/Cat.scss'
+import './assets/css/testimonal.scss'
 
 import './assets/css/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
