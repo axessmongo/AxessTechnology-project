b166f6b3deecea3c14abc3780d9574e0f3068755
diwaSelvam <devbharath21k@gmail.com>
2024-01-25 13:03:21 +0530

    added a new testing page

---
+++ client/src/components/Testingpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Testingpage() {
+  return (
+    <div>
+        <section className=''>
+            hiii hello
+        </section>
+    </div>
+  )
+}
+
+export default Testingpage
