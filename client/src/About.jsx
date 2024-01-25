dee5c1d1194cfbe2ed94aa7b136b076c62ea6d8a
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2024-01-25 18:08:27 +0530

    Added about page and import the routing in routing.jsx page

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 import AboutImage from '../src/assets/images/About2.jpg';
 
 function About() {
     return (
-        <div className='container'>
+        <div>
             <Navbar />
-            <h1 className='text-center mb-3'>About Us</h1>
-            <div className="row">
-                <div className="col-md-6">
-                    <h1>Heading</h1>
-                    <p className='mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ipsa corporis modi quam, quos illo. Debitis, facilis tenetur temporibus numquam maxime inventore nemo cum totam iure et, iusto unde magni obcaecati est iste quo vel distinctio beatae sequi hic quod aliquam nisi blanditiis. Sequi expedita a ea ducimus minus ipsa veritatis eligendi quidem cumque obcaecati nihil nostrum corrupti, sit deserunt hic voluptatibus possimus enim aperiam ipsam voluptatum maiores explicabo magni, earum libero. Ducimus minima vitae commodi saepe cupiditate dolore repellendus sapiente omnis sequi aut molestias debitis perspiciatis impedit et illum repellat, recusandae ipsam, voluptates tempora eligendi nulla vero quisquam fugiat.</p>
-                </div>
-                <div className="col-md-6">
-                    <div id="carouselExampleFade" className="carousel slide carousel-fade">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src={AboutImage} className="d-block w-100" alt="..." />
+            <Banner h1={'About Us'} h2={'Revolutionize Your Operations: Transform with Software Tailored to Your Needs.'} h3={'Hello World'}/>
+            <div className='container'>
+                <h1 className='text-center mb-3'>About Us</h1>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>Heading</h1>
+                        <p className='mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ipsa corporis modi quam, quos illo. Debitis, facilis tenetur temporibus numquam maxime inventore nemo cum totam iure et, iusto unde magni obcaecati est iste quo vel distinctio beatae sequi hic quod aliquam nisi blanditiis. Sequi expedita a ea ducimus minus ipsa veritatis eligendi quidem cumque obcaecati nihil nostrum corrupti, sit deserunt hic voluptatibus possimus enim aperiam ipsam voluptatum maiores explicabo magni, earum libero. Ducimus minima vitae commodi saepe cupiditate dolore repellendus sapiente omnis sequi aut molestias debitis perspiciatis impedit et illum repellat, recusandae ipsam, voluptates tempora eligendi nulla vero quisquam fugiat.</p>
+                    </div>
+                    <div className="col-md-6">
+                        <div id="carouselExampleFade" className="carousel slide carousel-fade">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src={AboutImage} className="d-block w-100" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    {/* <img src={AboutImage} className="d-block w-100" alt="..." /> */}
+                                    hsdjhbc
+                                </div>
+                                <div className="carousel-item">
+                                    {/* <img src={AboutImage} className="d-block w-100" alt="..." /> */}
+                                    gshsd
+                                </div>
                             </div>
-                            <div className="carousel-item">
-                                {/* <img src={AboutImage} className="d-block w-100" alt="..." /> */}
-                                hsdjhbc
-                            </div>
-                            <div className="carousel-item">
-                                {/* <img src={AboutImage} className="d-block w-100" alt="..." /> */}
-                                gshsd
-                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
                         </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
                     </div>
                 </div>
             </div>
--- client/src/pages/Technologies.jsx
+++ client/src/pages/Technologies.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 import "../assets/css/Technologies.scss"
-import '../assets/css/pricing.scss'
 import Footer from '../components/Footer'
 import techtools1 from "../assets/images/Technologies/techtools1.webp"
 // each tech image import----->
@@ -59,11 +58,6 @@ import xamarin from "../assets/images/Technologies/xamarin.webp"
 // each tech import ends---------->
 import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
-
-
-import american from '../assets/images/pricing/american.png'
-import master from '../assets/images/pricing/master.png'
-import visa from '../assets/images/pricing/visa.png'
 const Technologies = () => {
     const [TechActive, setTechActive] = useState(0);
 
@@ -438,84 +432,6 @@ const Technologies = () => {
                     </div>
                 </div>
             </div>
-            <section className='pricing mt-5'>
-				<h1 className='primary-header text-center'>Unlock Our New Features</h1>
-				<div className="container pricing-table">
-					<div className="pricing-card ">
-						<h3 className="pricing-card-header">Basic</h3>
-						<div className="price text-center"><sup>$</sup>15<span>/MO</span></div>
-						<ul>
-							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
-							<li><strong>Hosting - 1</strong> Year FREE</li>
-							<li><strong>Single 1 </strong> Page Website</li>
-							<li><strong>SSL</strong> Certificate</li>
-							<li><strong>WhatsApp </strong> Chat</li>
-							<li><strong>Social </strong> Media Integration</li>
-							<li><strong>Static </strong> Website</li>
-							<li><strong>Premade  </strong> Website</li>
-							<li><strong>3 Month FREE </strong> Tech Support</li>
-						</ul>
-						<a href="#" className="order-btn">Order Now</a>
-					</div>
-
-					<div className="pricing-card">
-						<h3 className="pricing-card-header">Pro</h3>
-						<div className="price text-center"><sup>$</sup>30<span>/MO</span></div>
-						<ul>
-							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
-							<li><strong>Hosting - 1</strong> Year FREE</li>
-							<li><strong>4 to 6</strong> Pages</li>
-							<li><strong>WhatsApp</strong> Chat</li>
-							<li><strong>SSL </strong> Certificate</li>
-							<li><strong>Full Source </strong> Code</li>
-							<li><strong>5 Email – 5GB </strong> Quota Each</li>
-							<li><strong>Premade  </strong> Website</li>
-							<li><strong>3 Month FREE </strong> Tech Support</li>
-						</ul>
-						<a href="#" className="order-btn">Order Now</a>
-					</div>
-
-					<div className="pricing-card">
-						<h3 className="pricing-card-header">Premium</h3>
-						<div className="price text-center"><sup>$</sup>50<span>/MO</span></div>
-						<ul>
-							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
-							<li><strong>Hosting - 1</strong> Year FREE</li>
-							<li><strong>4 to 6</strong> Pages</li>
-							<li><strong>WhatsApp</strong> Chat</li>
-							<li><strong>SSL </strong> Certificate</li>
-							<li><strong>Full Source </strong> Code</li>
-							<li><strong>5 Email – 5GB </strong> Quota Each</li>
-							<li><strong>Premade  </strong> Website</li>
-							<li><strong>3 Month FREE </strong> Tech Support</li>
-						</ul>
-						<a href="#" className="order-btn">Order Now</a>
-					</div>
-
-					<div className="pricing-card">
-						<h3 className="pricing-card-header">Elite</h3>
-						<div className="price text-center"><sup>$</sup>80<span>/MO</span></div>
-						<ul>
-							<li><strong>Domain</strong>- Additional Charges (Rs.800)</li>
-							<li><strong>Hosting - 1</strong> Year FREE</li>
-							<li><strong>4 to 6</strong> Pages</li>
-							<li><strong>WhatsApp</strong> Chat</li>
-							<li><strong>SSL </strong> Certificate</li>
-							<li><strong>Full Source </strong> Code</li>
-							<li><strong>5 Email – 5GB </strong> Quota Each</li>
-							<li><strong>Premade  </strong> Website</li>
-							<li><strong>3 Month FREE </strong> Tech Support</li>
-						</ul>
-						<a href="#" className="order-btn">Order Now</a>
-					</div>
-				</div>
-				<div className='Pay'>
-						<h5>ACCEPTED PAYMENT METHODS</h5>
-						<img src={visa} className='img-sizes' alt="visa" />
-						<img src={master} className='img-sizes' alt="master" />
-						<img src={american} className='img-sizes' alt="american" />
-					</div>
-			</section >
             <Footer />
         </div>
 
