2d313ddbb8b01e2dd75455d6a87596542aae595b
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2024-01-30 19:45:14 +0530

    axess page

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -499,7 +499,7 @@ function Services() {
                                         <img src={digi} className='w-100 animatedimg' alt="service" />
                                     </div>
                                 </div>
-
+                                    <br></br>
                                 <Link to={"/digitalmarketing"} className='gold-btn' data-aos='fade' data-aos-duration='600' data-aos-anchor-placement="bottom-bottom"><span className='d-block'>Know More</span> <i className="bi bi-arrow-right"></i></Link>
                             </div>
                         </div>
