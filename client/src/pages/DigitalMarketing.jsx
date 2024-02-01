44312ef477cba7803aa299d9387042af4e4310ae
diwaSelvam <devbharath21k@gmail.com>
2024-02-01 11:08:30 +0530

    some change and edit pages

--- client/src/pages/DigitalMarketing.jsx
+++ client/src/pages/DigitalMarketing.jsx
@@ -235,6 +235,172 @@ export default function DigitalMarketing() {
           </section>
         </div>
 
+         {/* Price section */}
+      <section className="pricing mt-5" id="pricing-section">
+        <h1 className="primary-header text-center">Unlock Our New Features</h1>
+        <div className="container pricing-table">
+          <div className="pricing-card ">
+            <h3 className="pricing-card-header">Basic</h3>
+            <div className="price text-center">
+              <sup>$</sup>15<span>/MO</span>
+            </div>
+            <ul>
+              <li>
+                <strong>Domain</strong>- Additional Charges (Rs.800)
+              </li>
+              <li>
+                <strong>Hosting - 1</strong> Year FREE
+              </li>
+              <li>
+                <strong>Single 1 </strong> Page Website
+              </li>
+              <li>
+                <strong>SSL</strong> Certificate
+              </li>
+              <li>
+                <strong>WhatsApp </strong> Chat
+              </li>
+              <li>
+                <strong>Social </strong> Media Integration
+              </li>
+              <li>
+                <strong>Static </strong> Website
+              </li>
+              <li>
+                <strong>Premade </strong> Website
+              </li>
+              <li>
+                <strong>3 Month FREE </strong> Tech Support
+              </li>
+            </ul>
+            <a href="#" className="order-btn">
+              Order Now
+            </a>
+          </div>
+
+          <div className="pricing-card">
+            <h3 className="pricing-card-header">Pro</h3>
+            <div className="price text-center">
+              <sup>$</sup>30<span>/MO</span>
+            </div>
+            <ul>
+              <li>
+                <strong>Domain</strong>- Additional Charges (Rs.800)
+              </li>
+              <li>
+                <strong>Hosting - 1</strong> Year FREE
+              </li>
+              <li>
+                <strong>4 to 6</strong> Pages
+              </li>
+              <li>
+                <strong>WhatsApp</strong> Chat
+              </li>
+              <li>
+                <strong>SSL </strong> Certificate
+              </li>
+              <li>
+                <strong>Full Source </strong> Code
+              </li>
+              <li>
+                <strong>5 Email – 5GB </strong> Quota Each
+              </li>
+              <li>
+                <strong>Premade </strong> Website
+              </li>
+              <li>
+                <strong>3 Month FREE </strong> Tech Support
+              </li>
+            </ul>
+            <a href="#" className="order-btn">
+              Order Now
+            </a>
+          </div>
+
+          <div className="pricing-card">
+            <h3 className="pricing-card-header">Premium</h3>
+            <div className="price text-center">
+              <sup>$</sup>50<span>/MO</span>
+            </div>
+            <ul>
+              <li>
+                <strong>Domain</strong>- Additional Charges (Rs.800)
+              </li>
+              <li>
+                <strong>Hosting - 1</strong> Year FREE
+              </li>
+              <li>
+                <strong>4 to 6</strong> Pages
+              </li>
+              <li>
+                <strong>WhatsApp</strong> Chat
+              </li>
+              <li>
+                <strong>SSL </strong> Certificate
+              </li>
+              <li>
+                <strong>Full Source </strong> Code
+              </li>
+              <li>
+                <strong>5 Email – 5GB </strong> Quota Each
+              </li>
+              <li>
+                <strong>Premade </strong> Website
+              </li>
+              <li>
+                <strong>3 Month FREE </strong> Tech Support
+              </li>
+            </ul>
+            <a href="#" className="order-btn">
+              Order Now
+            </a>
+          </div>
+
+          <div className="pricing-card">
+            <h3 className="pricing-card-header">Elite</h3>
+            <div className="price text-center">
+              <sup>$</sup>80<span>/MO</span>
+            </div>
+            <ul>
+              <li>
+                <strong>Domain</strong>- Additional Charges (Rs.800)
+              </li>
+              <li>
+                <strong>Hosting - 1</strong> Year FREE
+              </li>
+              <li>
+                <strong>4 to 6</strong> Pages
+              </li>
+              <li>
+                <strong>WhatsApp</strong> Chat
+              </li>
+              <li>
+                <strong>SSL </strong> Certificate
+              </li>
+              <li>
+                <strong>Full Source </strong> Code
+              </li>
+              <li>
+                <strong>5 Email – 5GB </strong> Quota Each
+              </li>
+              <li>
+                <strong>Premade </strong> Website
+              </li>
+              <li>
+                <strong>3 Month FREE </strong> Tech Support
+              </li>
+            </ul>
+            <a href="#" className="order-btn">
+              Order Now
+            </a>
+          </div>
+          {/* <div className='Pay'>
+						<h3>ACCEPTED PAYMENT METHODS</h3>
+
+					</div> */}
+        </div>
+      </section>
+
         <div>
         <section className='nav-banner testimonal' id="testimonial">
           <div>
