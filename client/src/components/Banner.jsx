70699c95010e345679b67c75fbf0e8d9ab526e59
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2024-02-02 13:22:36 +0530

    contact accordion

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Banner({h1,h2,h3}) {
     return (
@@ -7,7 +8,7 @@ export default function Banner({h1,h2,h3}) {
                 <p className="bg-img-2 mb-5 mx-auto" data-aos='fade-up' data-aos-duration='400'>{h1}</p>
                 <h1 className='secondary-header mb-3' data-aos='fade-up' data-aos-duration='600'>{h2}</h1>
                 <p className='mb-0 secondary-text mb-3' data-aos='fade-up' data-aos-duration='700'>{h3}</p>
-                <button className='gold-btn green-btn m-5 mx-auto' data-aos='zoom-out' data-aos-duration='600'><span className='d-block'>Connect with us</span> <i className="bi bi-arrow-right"></i></button>
+              <Link to={"/contact"} className='text-decoration-none'> <button className='gold-btn green-btn m-5 mx-auto' data-aos='zoom-out' data-aos-duration='600'><span className='d-block'>Connect with us</span> <i className="bi bi-arrow-right"></i></button></Link> 
             </div>
         </div>
     )
