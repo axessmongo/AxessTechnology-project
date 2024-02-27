670fcd0b1c375d54cc24d832eb8ce44c49e22e78
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2024-02-27 13:40:28 +0530

    digi contact

--- client/src/components/Digitalcontact.jsx
+++ client/src/components/Digitalcontact.jsx
@@ -142,7 +142,7 @@ function Digitalcontact() {
     }
     console.log("before try")
     try {
-      const response = await axios.post("http://localhost:5000/digital/contact", state);
+      const response = await axios.post("/digital/contact", state);
 
       if (response.status === 201) {
         showToast("Our team will connect with you", { style: { background: "linear-gradient(to right, #00b09b, #96c93d)" } });
