eb39b2482d97d7bbd8d5997419a0746472458ac0
devbharath21k <devbharath21k@gmail.com>
2024-03-11 19:27:23 +0530

    'new'

--- backend/userModel/ContactSchema.js
+++ backend/userModel/ContactSchema.js
@@ -4,37 +4,39 @@ const mongoose = require("mongoose");
 // Destructuring Schema from mongoose
 const { Schema } = mongoose;
 
-// Defining the schema for the contact
 const contactSchema = new Schema(
   {
     fname: {
       type: String, // Field for first name, expects a string
+      required: true // Making first name required
     },
     email: {
       type: String, // Field for email, expects a string, and should be unique
       unique: true,
+      required: true // Making email required
     },
     phone: {
       type: String, // Field for phone number, expects a string
+      required: true // Making phone number required
     },
     website: {
-      type: String, // Field for website, expects a string
+      type: String // Field for website, expects a string
     },
     company: {
-      type: String, // Field for company, expects a string
+      type: String // Field for company, expects a string
     },
-    services: {}, // Field for services, but the type is not defined. Assuming it might be an object or array, you should specify the type.
-
+    services: {
+      type: Schema.Types.Mixed 
+    },
     digitalmarketBudget: {
-      type: String, // Field for digital market budget, expects a string
+      type: String // Field for digital market budget, expects a string
     },
     comments: {
-      type: String, // Field for comments, expects a string
+      type: String // Field for comments, expects a string
     },
   },
   { timestamps: true } // Adding timestamps for createdAt and updatedAt
 );
-
 // Creating a model named "DigitalWorld" using the contact schema
 const Contact = mongoose.model("DigitalWorld", contactSchema);
 
