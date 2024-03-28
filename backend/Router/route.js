26657e1600df41b7c5f74e8230a1bf4be0c0e551
devbharath21k <devbharath21k@gmail.com>
2024-03-28 11:50:11 +0530

    new

--- backend/Router/route.js
+++ backend/Router/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AxessTechnology =require("../TaskControl/NormalControl.js");
 const DigitalContact =require("../TaskControl/HomepageControl.js");
 
-router.post("/api/contact", AxessTechnology.postNormalContact);
+router.post("/api/call", AxessTechnology.postNormalContact);
 router.post("/api/digital", DigitalContact.Contactpostmethod);
 
 
